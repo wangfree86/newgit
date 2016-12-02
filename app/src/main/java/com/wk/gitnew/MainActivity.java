45c9910bfbd029896b7af1971cb779d922d465fc
wang <wangfree86.@gmail.com>
2016-12-02 20:13:33 +0800

    test33

--- app/src/main/java/com/wk/gitnew/MainActivity.java
+++ app/src/main/java/com/wk/gitnew/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //test2222222222222
+        //33333333333
     }
 }
